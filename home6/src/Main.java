a8b67796e321db465cc33875dc384710a335e3dc
78VS78 <vsergey0578@gmail.com>
2023-01-07 13:45:16 +0300

    OOP intro

---
+++ home6/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        double[] ar = {-56, 567, 23, 1, 0, 121.121, 17, 22.555555555555, 19999, 78};
+
+        Arr arr1 = new Arr(ar);
+        arr1.Min_max();
+        arr1.Longes();
+        arr1.Chetn();
+        arr1.Puzir();
+
+    }
+}
